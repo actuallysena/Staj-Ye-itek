5b3ab548170484d693625fecad642f1d8cf63c34
sena <actuallysena@gmail.com>
2024-12-18 13:42:02 +0300

    first look

---
+++ kurumsalsite_1.gün/index.php
@@ -0,0 +1,119 @@
+<!DOCTYPE html
+  PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+
+<head>
+  <meta charset="utf-8">
+  <title>Kurumsal Web Site</title>
+
+
+  <!-- Fontlar -->
+  <link
+    href="https://fonts.googleapis.com/css?family=Open+Sans:300,300i,400,400i,700,700i|Raleway:300,400,500,700,800|Montserrat:300,400,700"
+    rel="stylesheet">
+
+  <!-- Bootstrap stil dosyası -->
+  <link href="lib/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+
+  <!-- işimize yarayacak diğer kütüphane css dosyalarımız -->
+  <link href="lib/font-awesome/css/font-awesome.min.css" rel="stylesheet">
+  <link href="lib/animate/animate.min.css" rel="stylesheet">
+  <link href="lib/ionicons/css/ionicons.min.css" rel="stylesheet">
+  <link href="lib/owlcarousel/assets/owl.carousel.min.css" rel="stylesheet">
+  <link href="lib/magnific-popup/magnific-popup.css" rel="stylesheet">
+  <link href="lib/ionicons/css/ionicons.min.css" rel="stylesheet">
+
+  <!-- bizim stil dosyamız -->
+  <link href="css/style.css" rel="stylesheet">
+
+
+</head>
+
+<body>
+
+  <!----------Üst Bar---------->
+
+  <section id="topbar" class d="d-none d-lg-block">
+    <div class="container clearfix">
+
+      <div class="contact-info float-left">
+        <i class="fa fa-envelope-o"></i><a href="mailto: info@udemykurumsal.com">info@udemykurumsal.com</a>
+        <i class="fa fa-phone"></i> +90 5xx xxx xx xx
+      </div>
+
+
+      <div class="social_links float-right">
+        <a href="#" class="twitter"><i class="fa fa-twitter"></i></a>
+        <a href="##" class="facebook"><i class="fa fa-facebook"></i></a>
+        <a href="###" class="instagram"><i class="fa fa-instagram"></i></a>
+        <a href="####" class="linkedin"><i class="fa fa-linkedin"></i></a>
+        <a href="#####" class="github"><i class="fa fa-github"></i></a>
+      </div>
+    </div>
+  </section>
+
+  <!-------- header -------->
+
+  <header id="header">
+    <div class="container">
+      <div id="logo" class="pull-left">
+        <h1><a href="#body" class="scrollto">Udemy <span> Kurumsal</span></a></h1>
+
+      </div>
+
+      <nav id="nav-menu-container">
+        <ul class="nav-menu">
+
+          <li class="menu-active"><a href="#body"> Anasayfa</a></li>
+          <li class="menu-active"><a href="#hakkimizda"> Hakkımızda</a></li>
+          <li class="menu-active"><a href="#hizmetlerimiz"> Hizmetlerimiz</a></li>
+          <li class="menu-active"><a href="#filomuz">Araç Filomuz</a></li>
+          <li class="menu-active"><a href="#iletisim"> İletişim</a></li>
+        </ul>
+      </nav>
+    </div>
+  </header>
+
+
+  <!--------- INTRO --------->
+
+  <section id="intro">
+
+    <div class="intro-content">
+      <h2> Güvenle...</h2>
+
+    </div>
+
+    <div id="intro-carousel" class="owl-carousel">
+      <div class="item" style="background-image:url('img/carousel/1.jpg');"></div>
+      <div class="item" style="background-image:url('img/carousel/2.jpg');"></div>
+      <div class="item" style="background-image:url('img/carousel/3.jpg');"></div>
+      <div class="item" style="background-image:url('img/carousel/4.jpg');"></div>
+      <div class="item" style="background-image:url('img/carousel/5.jpg');"></div>
+    </div>
+
+  </section> id="hakkimizda"
+
+
+
+
+
+
+
+
+  <!-- Kütüphaneler -->
+  <script src="lib/jquery/jquery.min.js"></script>
+  <script src="lib/jquery/jquery-migrate.min.js"></script>
+  <script src="lib/bootstrap/js/bootstrap.bundle.min.js"></script>
+  <script src="lib/easing/easing.min.js"></script>
+  <script src="lib/superfish/hoverIntent.js"></script>
+  <script src="lib/superfish/superfish.min.js"></script>
+  <script src="lib/wow/wow.min.js"></script>
+  <script src="lib/owlcarousel/owl.carousel.min.js"></script>
+  <script src="lib/magnific-popup/magnific-popup.min.js"></script>
+  <script src="lib/sticky/sticky.js"></script>
+  <script src="js/main.js"></script>
+
+</body>
+
+</html>
